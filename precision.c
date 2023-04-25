ec9275648eb9ab9ba99529484ccdd3fc87f8c0fa
Maryannewambui <trimbleleewambui@gmail.com>
2023-04-25 11:11:25 -0700

    helper functions

--- precision.c
+++ precision.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
+ * get_precision - a function that calculates the precision for printing
+ * @format: Formatted string
  * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @list: list
  *
  * Return: Precision.
  */
